f0ea7b410d30ad11e54c1b6841a9953ed70f23b3
Reyoth <serialkiwi@gmail.com>
2017-01-14 16:10:09 +0100

    debut du webshop

---
+++ WebShop.Web/WebShop.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebShop.Web.Startup))]
+namespace WebShop.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
