df44e0d6a1ca99645990514d30912046b622f3af
Reyoth <serialkiwi@gmail.com>
2017-01-16 20:55:20 +0100

    avant le grid mvc.ajax

--- WebShop.Web/WebShop.Web/ApiControllers/ArticlesController.cs
+++ WebShop.Web/WebShop.Web/ApiControllers/ArticlesController.cs
@@ -1,18 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Mvc;
+using WebShop.Domain;
 
 namespace WebShop.Web.ApiControllers
 {
     public class ArticlesController : ApiController
     {
-        public object Search(int category, int subCategory)
+        public object Search(int categoryId, int subCategoryId)
         {
-
+            var context = new WebShopEntities();
+            var articles = context.Articles
+                .Include(a => a.SousCategorie)
+                .Where(a => (a.ART_SCAT_Id == subCategoryId || subCategoryId == -2)
+                            && (a.SousCategorie.SCAT_CAT_Id == categoryId || categoryId == -2));
+            return articles;
         }
     }
 }
